7c0d4cd68e76cc4045222156159a0596bc8d23b2
kauangouveia <kauangouveia64@gmail.com>
2022-10-22 11:19:27 -0300

    first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px gray;
+  }
+
+  body {
+    background: #121214;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    -webkit-font-smoothing : antialiased;
+  }
+`
